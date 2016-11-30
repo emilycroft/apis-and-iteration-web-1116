3551788aa970e27d56d278d2ce86da6ea7e3c732
emilycroft <emilyhcroft@gmail.com>
2016-11-30 14:28:08 -0500

    Done.

--- lib/api_communicator.rb
+++ lib/api_communicator.rb
@@ -6,7 +6,22 @@ def get_character_movies_from_api(character)
   #make the web request
   all_characters = RestClient.get('http://www.swapi.co/api/people/')
   character_hash = JSON.parse(all_characters)
-  
+  characters = character_hash["results"]
+
+  array_of_film_hashes = []
+
+  characters.each do |character_hash|
+    array_of_film_hashes = character_hash["films"] if character_hash["name"].downcase == character
+  end
+  array_of_film_hashes.map! do |url|
+    url = JSON.parse(RestClient.get(url))
+  end
+
+  # make a conditional that checks character_hash["results"], which is an array of character hashes
+  # iterate over that array and check the name key
+  # if the name key == user input
+  # return the array corresponding to the films key
+
   # iterate over the character hash to find the collection of `films` for the given
   #   `character`
   # collect those film API urls, make a web request to each URL to get the info
@@ -19,6 +34,11 @@ def get_character_movies_from_api(character)
 end
 
 def parse_character_movies(films_hash)
+  titles = []
+  films_hash.each do |film|
+    titles << film["title"]
+  end
+  puts "This character appears in #{titles[0...-1].join(", ")}, and #{titles.last}."
   # some iteration magic and puts out the movies in a nice list
 end
 
